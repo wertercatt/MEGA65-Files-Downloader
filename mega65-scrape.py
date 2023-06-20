47eb0b854cd5f2bed116eeb7672b557cc269aab9
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-06-19 22:56:54 -0500

    Print relative filepaths

--- mega65-scrape.py
+++ mega65-scrape.py
@@ -7,3 +7,6 @@ fileJSON = json.loads(requests.get(readfilespublic).text)
 
 jsonbackup = open("./output/readfiles.json", "w")
 json.dump(fileJSON, jsonbackup, sort_keys=True, indent=4)
+
+for _ in trange(len(fileJSON), desc="Getting Files"):
+    print(fileJSON[_]['location'])
