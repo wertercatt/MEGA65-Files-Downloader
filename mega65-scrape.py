c0f8d22e95e0700ae93b0e19962701ee69a3e257
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-06-19 22:52:39 -0500

    Backup file.

--- mega65-scrape.py
+++ mega65-scrape.py
@@ -4,4 +4,6 @@ from tqdm.auto import trange
 from pathvalidate import sanitize_filepath
 readfilespublic = "https://files.mega65.org/php/readfilespublic.php"
 fileJSON = json.loads(requests.get(readfilespublic).text)
-print(json.dumps(fileJSON, sort_keys=True, indent=4))
+
+jsonbackup = open("./output/readfiles.json", "w")
+json.dump(fileJSON, jsonbackup, sort_keys=True, indent=4)
