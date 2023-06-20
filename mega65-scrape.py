c806e85abfb21c7fde32cb6e9a1a7f088628e879
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-06-19 23:11:06 -0500

    Working

--- mega65-scrape.py
+++ mega65-scrape.py
@@ -2,11 +2,26 @@ import requests
 import json
 from tqdm.auto import trange
 from pathvalidate import sanitize_filepath
+import os
+
 readfilespublic = "https://files.mega65.org/php/readfilespublic.php"
 fileJSON = json.loads(requests.get(readfilespublic).text)
-
+os.makedirs(sanitize_filepath("output"), exist_ok=True)
 jsonbackup = open("./output/readfiles.json", "w")
 json.dump(fileJSON, jsonbackup, sort_keys=True, indent=4)
 
 for _ in trange(len(fileJSON), desc="Getting Files"):
-    print(fileJSON[_]['location'])
+    LocalFileLocation = sanitize_filepath("output/" + fileJSON[_]['location'])
+    os.makedirs("/".join(LocalFileLocation.split("/"))[:-1], exist_ok=True)
+    with requests.get("https://files.mega65.org/" + fileJSON[_]['location']) as RawFileContent, open(
+        LocalFileLocation, "wb"
+        ) as LocalFile, open(
+            sanitize_filepath(LocalFileLocation + ".headers.json"), "w"
+        ) as LocalContentHeadersOutput:
+            LocalFile.write(RawFileContent.content)
+            json.dump(
+                dict(RawFileContent.headers),
+                LocalContentHeadersOutput,
+                sort_keys=True,
+                indent=4,
+            )
