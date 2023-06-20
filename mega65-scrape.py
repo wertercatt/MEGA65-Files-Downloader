8e2ac4e8e2244c5a4b1c1904455532615785976c
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-06-19 23:18:08 -0500

    Add newlines

--- mega65-scrape.py
+++ mega65-scrape.py
@@ -11,7 +11,7 @@ jsonbackup = open("./output/readfiles.json", "w")
 json.dump(fileJSON, jsonbackup, sort_keys=True, indent=4)
 
 for _ in trange(len(fileJSON), desc="Getting Files"):
-    print(fileJSON[_]['title'])
+    print("\n" + fileJSON[_]['title'] + "\n")
     LocalFileLocation = sanitize_filepath("output/" + fileJSON[_]['location'])
     os.makedirs("/".join(LocalFileLocation.split("/")[:-1]), exist_ok=True)
     with requests.get("https://files.mega65.org/" + fileJSON[_]['location']) as RawFileContent, open(
