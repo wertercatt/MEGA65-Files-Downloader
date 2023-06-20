99c84988c86ff759480978481b150e0955605d79
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-06-19 22:47:25 -0500

    Load file

--- mega65-scrape.py
+++ mega65-scrape.py
@@ -3,3 +3,5 @@ import json
 from tqdm.auto import trange
 from pathvalidate import sanitize_filepath
 readfilespublic = "https://files.mega65.org/php/readfilespublic.php"
+fileJSON = json.loads(requests.get(readfilespublic).text)
+print(json.dumps(fileJSON, sort_keys=True, indent=4))
